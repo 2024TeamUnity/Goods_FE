6101300bd4a86a401789a0af152bf9c10b45852c
DxxxHxx <donghun1163@gmail.com>
2024-06-05 16:54:31 +0900

    Feat: 관심 목록 무한 스크롤

--- src/routes/WishHistory.tsx
+++ src/routes/WishHistory.tsx
@@ -1,9 +1,64 @@
-import CardListPage from '../components/common/CardListPage';
+import { useMemo } from 'react';
 import LoadingSpinner from '../components/common/LoadingSpinner';
-import { useWishHistoryQuery } from '../service/mypage/useWishHistoryQuery';
+import { usePaginatedHistoryQuery } from '../service/mypage/useWishHistoryQuery';
+import Observer from '../components/common/Observer';
+import { Link } from 'react-router-dom';
+import { getTime } from '../util/getTime';
+import AddWishListButton from '../components/common/AddWishListButton';
+import HistoryPageContainer from '../components/common/HistoryPageContainer';
 
 export default function WishHistory() {
-  const { data, isLoading } = useWishHistoryQuery();
+  const { data, isLoading, hasNextPage, fetchNextPage } = usePaginatedHistoryQuery();
+
+  const wishList = useMemo(() => {
+    if (data) {
+      return data.pages.reduce((acc, cur) => [...acc, ...cur], []);
+    }
+  }, [data]);
+
+  const addComma = (price: string): string => {
+    const commaPrice = price.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return commaPrice;
+  };
+
   if (isLoading) return <LoadingSpinner />;
-  return <CardListPage data={data!} title='관심 목록' />;
+  return (
+    <HistoryPageContainer title='관심 목록' isEmpty={!!!wishList?.length}>
+      {wishList?.map((item) => (
+        <li
+          key={item.goods_id}
+          className='flex flex-col w-full transition-all duration-200 scale-95 border rounded-xl gap-y-3 hover:scale-105 '
+        >
+          <Link
+            to={`/posts/${item.goods_id}`}
+            className='flex items-center justify-start p-4 gap-x-8'
+          >
+            <img
+              className='object-cover w-24 h-24 rounded-xl md:w-32 md:h-32'
+              src={item.goods_thumbnail}
+              alt='img'
+            />
+            <div className='relative flex flex-col flex-1 py-2'>
+              <p className='text-lg font-bold'>{item.goods_name}</p>
+              <p className='flex-1 mt-2 mb-4 text-sm text-neutral-500'>
+                {getTime(item.uploaded_before!)}
+              </p>
+              <div className='flex items-center justify-start text-base gap-x-2'>
+                <div className='flex items-center justify-center w-16 p-1 text-sm text-white bg-secondary rounded-xl'>
+                  {item.goods_status}
+                </div>
+                <span className='font-bold'>{addComma(String(item.price))}원</span>
+              </div>
+
+              <div className='absolute right-2 top-2'>
+                <AddWishListButton goodsId={item.goods_id} wish />
+              </div>
+            </div>
+          </Link>
+        </li>
+      ))}
+
+      <Observer hasNext={hasNextPage} loadMore={fetchNextPage} />
+    </HistoryPageContainer>
+  );
 }
--- src/service/mypage/useWishHistoryQuery.ts
+++ src/service/mypage/useWishHistoryQuery.ts
@@ -1,5 +1,5 @@
-import { useQuery } from '@tanstack/react-query';
-import { IWishHistoryResponse } from '../../types/interface';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { IWishHistoryData, IWishHistoryResponse } from '../../types/interface';
 import client from '../../util/authAxios';
 
 export const useWishHistoryQuery = () => {
@@ -8,4 +8,17 @@ export const useWishHistoryQuery = () => {
     queryFn: async () => (await client.get('/api/goods/likes')).data,
   });
   return { data, isLoading };
+};
+
+export const usePaginatedHistoryQuery = () => {
+  const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ['wishList'],
+    queryFn: async ({ pageParam }: { pageParam: number }) =>
+      (await client.get(`/api/goods/likes`, { params: { page: pageParam } })).data
+        .content as IWishHistoryData[],
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, _, lastPageParams) =>
+      lastPage.length ? lastPageParams + 1 : undefined,
+  });
+  return { data, isLoading, fetchNextPage, hasNextPage };
 };
