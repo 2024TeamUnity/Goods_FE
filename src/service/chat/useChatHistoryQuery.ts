23d9255a7af4cc1b6a9b53bc5faaebf7fcea80ae
DxxxHxx <donghun1163@gmail.com>
2024-06-13 20:53:29 +0900

    Refactor: 채팅방 나가기 UI 수정

--- src/service/chat/useChatHistoryQuery.ts
+++ src/service/chat/useChatHistoryQuery.ts
@@ -1,5 +1,5 @@
-import { useQuery } from '@tanstack/react-query';
-import { IChatHistoryData } from '../../types/interface';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { IChatHistoryData, IChatLog } from '../../types/interface';
 import client from '../../util/authAxios';
 
 export const useChatHistoryQuery = (roomId: string) => {
@@ -7,5 +7,24 @@ export const useChatHistoryQuery = (roomId: string) => {
     queryKey: ['chatHistory', `${roomId}`],
     queryFn: async () => (await client.get(`/api/chat/${roomId}`)).data,
   });
+
   return { data, isLoading };
+};
+
+export const useChatLog = (roomId: string) => {
+  const {
+    data: chatLogData,
+    isLoading: chatLogLoading,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    queryKey: ['chatLog', roomId],
+    queryFn: async ({ pageParam }: { pageParam: number }) =>
+      (await client.get(`/api/chat/${roomId}`, { params: { page: pageParam } })).data.chat_logs
+        .content as IChatLog[],
+    initialPageParam: 2,
+    getNextPageParam: (lastPage, _, lastPageParam) =>
+      lastPage.length ? lastPageParam + 1 : undefined,
+  });
+  return { chatLogData, chatLogLoading, hasNextPage, fetchNextPage };
 };
